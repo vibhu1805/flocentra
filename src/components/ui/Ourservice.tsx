aa45fe70e3b42be8e665491717c646957baa2caa
VaishnaviiMishra <146117422+VaishnaviiMishra@users.noreply.github.com>
2024-03-16 19:15:34 +0530

    More Chanes

--- src/components/ui/Ourservice.tsx
+++ src/components/ui/Ourservice.tsx
@@ -1,47 +1,63 @@
 import React, {useState} from 'react';
-export const About = () => {
-    const Aboutst:React.CSSProperties = {
+export const Ourservice = () => {
+    const Servicest:React.CSSProperties = {
         fontFamily:"Benne",
         fontSize:"25px",
         color:"#23363E"
     }
 
-    const servicestl:React.CSSProperties={
-        fontFamily:"Inter",
-        fontSize:"20px"
-    }
+   
     return(
         
-        <div style={Aboutst}>
-            <img> </img>
-            <div>                
-                <p>
-                Services
-                </p>
-                <h4>
-                    Mr. Dhiraj Kumar
-                </h4>
-                <h6>
-                    Managing Director and CEO
-                </h6>
-            </div>
-            <div>
-                <p>
-                "Mrs. SUDHA MISHRA, The founder, Chairperson and woman Director of the company, post graduated in the English and has more than 25 years of experience as an entrepreneur and an excellent management consultant in different industries including aviation.
-                Expert in understanding and delivering business concepts in flashy and never-forgettable manner. A highly creative, energetic, customer-focused professional with progressive experience. 
-                Adept in planning and executing aggressive strategies to achieve business goals."
-                </p>
-                <h4>
-                    Mrs. Sudha Mishra
-                </h4>
-                <h6>
-                    Founder, Chairperson and Woman Director
-                </h6>
-            </div>
-
-
-
-            
+        <div style={Servicest}>
+            <h1>
+                Our Services
+            </h1>
+            <h4>
+                Overflight Permits & Landing Permits
+            </h4>
+            <p>
+            We offer essential permit assistance for flights, providing 24/7 support to secure overflight and landing permits at short notice. 
+            Our operations team monitors flights for safety, and our direct connections with global Civil Aviation Authorities ensure fast permit acquisition, 
+            especially in Asia, the Middle East, Africa, and CIS countries.
+            </p>
+            <h4>
+                Global Ground Handling Arrangements
+            </h4>
+            <p>
+            Our extensive ground handling network comprises proactive local representatives worldwide, enabling us to provide seamless flight operations from arrival to departure. 
+            We continuously monitor flights to ensure top-notch services for our valuable customers.
+            </p>
+            <h4>
+                Aircraft Fuel Uplift Arrangements
+            </h4>
+            <p>
+            We have wide range of fuel suppliers across the globe in order to provide best comprehensive fuel prices of " Jet A1, TS1, Avgas/100LL " for our valuable customers,
+            Our wide range of customer are taking our fuel services as they known about our commitments to words our customers.
+            </p>
+            <h4>
+                Crew Hotel Reservations
+            </h4>
+            <p>
+            It is one of our core responsibilities to provide safe shelter to our customers where they can avail all of the prime facilities within reasonable prices. 
+            As we have large growing business with the hotels, we may get some special prices as compare to others which would differentiate us.
+            </p>
+            <h4>
+                Crew & Passenger Transportations
+            </h4>
+            <p>
+            We provide an exclusive premium service catering to airline crew, VIPs, business personnel, and private jet passengers who need efficient and timely service. 
+            We offer a range of luxury vehicles and partner with trusted companies for seamless transportation.
+            All vehicles are immaculately maintained, clean, and the latest models.
+            </p>
+            <h4>
+                Credit Available
+            </h4>
+            <p>
+            We provide credit options for all our valued customers at airports worldwide, allowing us to negotiate rates with local representatives. 
+            After the trip, our accounts department issues invoices with supporting documents to reduce costs and promote transparency.
+            Our aim is to build stronger and more reliable relationships with our clients.  
+            </p>            
         </div>
 
     )
